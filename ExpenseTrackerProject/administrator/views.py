1fcf5be3c7fb837f3c6bbb038efb1cc62c803f59
Arjun Shome <arjunshome111@gmail.com>
2019-04-24 01:44:10 +0530

    Revert "Importing Excel and Loading into DB" (#4)

--- ExpenseTrackerProject/administrator/views.py
+++ ExpenseTrackerProject/administrator/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from administrator.readAndUpload import importExcelAndLoad
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hi, This is learning of Django.")
-
-def dbUpload(request):
-    load = importExcelAndLoad()
-    load.importExcel()
-    #return HttpResponse("Congrats :), Database got updated with the latest Expense records.")
-    return HttpResponse("Congrats :), Read Completed.")
+    return HttpResponse("Hi, This is learning of Django")
