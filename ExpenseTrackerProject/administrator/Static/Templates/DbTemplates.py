89e274233301a49720c6369bfafd9e12305b1ece
Arjun Shome <arjunshome111@gmail.com>
2019-04-28 00:21:33 +0530

    JSon Load from Db Record and Show in View (#10)

--- ExpenseTrackerProject/administrator/Static/Templates/DbTemplates.py
+++ ExpenseTrackerProject/administrator/Static/Templates/DbTemplates.py
@@ -3,8 +3,25 @@ def GetQuery(command):
     """Gives the Query template to the user for the required db command."""
 
     QueryTemplate = {
-        "insert":"INSERT INTO {table} VALUES {data}",
         "update":"UPDATE {table} SET {column} = {value} WHERE {condition}",
         "delete":"DELETE FROM {table} WHERE {condition}"
     }
-    return QueryTemplate[command]
+    return QueryTemplate.get(command)
+
+
+def GetQueryRecords(QyeryType):
+    """Get Type Of SELECT Queries"""
+
+    SelectQueryTemplate = {
+        "fetchAll":"SELECT * FROM {TableName}",
+        "fetch_WithCondition":"SELECT * FROM {TableName} WHERE {ColumnName} = {Value}"
+    }
+    return SelectQueryTemplate.get(QyeryType)
+
+def GetTable(TableName):
+    """Get The Table Name"""
+
+    Tables={
+        "data":"TbExpenseRecord"
+    }
+    return Tables.get(TableName)
--- ExpenseTrackerProject/administrator/urls.py
+++ ExpenseTrackerProject/administrator/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('index/',views.index, name = 'index'),
     path('dbinsert/',views.Read_Excel_LoadDB, name = 'dbinsert'),
-    path('jsonresp/',views.Read_Json, name = 'jsonresp')
+    path('jsonresp/',views.Read_Json, name = 'jsonresp'),
+    path('getdbdataJson/',views.GetDataFromDB, name = 'getdbdataJson')
 ] 
